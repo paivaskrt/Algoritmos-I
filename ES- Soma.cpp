bf6585e3afff111ac67a3349f687272a401680fd
João Pedro Chiachio de Paiva <chiachioo04@gmail.com>
2024-09-08 04:30:04 -0300

    Add files via upload

---
+++ ES- Soma.cpp
@@ -0,0 +1,16 @@
+ #include <iostream>
+ using namespace std;
+ 
+  int main ()
+{
+    int A;
+    int B;
+    int X;
+    
+    cin >> A >> B;
+    X = A + B;
+    cout << "X = " << X << endl;
+
+
+    return 0;
+}
